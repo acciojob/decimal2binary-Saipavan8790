933d0910d774ab0f36226b24a351e50a48c17034
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-09-14 03:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function decimalToBinary(num) {
   //Write you code here
-	 if (decimal === 0) {
+	 if (num === 0) {
     return "0"; // Special case for decimal 0
   }
 
   let binary = ""; // Initialize an empty string to store binary digits
 
-  while (decimal > 0) {
-    const remainder = decimal % 2; // Calculate the remainder
+  while (num > 0) {
+    const remainder = num % 2; // Calculate the remainder
     binary = remainder + binary; // Add the remainder to the beginning of the binary string
-    decimal = Math.floor(decimal / 2); // Update the decimal number by dividing it by 2 (integer division)
+    num = Math.floor(num / 2); // Update the decimal number by dividing it by 2 (integer division)
   }
 
   return binary;
   
 }
+// Test cases
+console.log(decimalToBinary(7));  // Output: "111"
+console.log(decimalToBinary(10)); // Output: "1010"
+console.log(decimalToBinary(33)); // Output: "100001"
 
 window.decimalToBinary = decimalToBinary;
